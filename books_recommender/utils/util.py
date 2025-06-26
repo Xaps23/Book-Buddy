13983c490c2964e3bcc805190da845b9891201da
Xaps23 <sarwashree23@gmail.com>
2025-06-26 15:33:42 +0545

    utils, exception and logger added

--- books_recommender/utils/util.py
+++ books_recommender/utils/util.py
@@ -3,7 +3,7 @@ import sys
 from books_recommender.exception.exception_handler import AppException
 
 
-def read_yaml_file(file_path: str) -> dict:
+def read_yaml_file(file_path:str)-> dict:
     """
     Reads a YAML file and returns its content as a dictionary.
     
@@ -11,7 +11,7 @@ def read_yaml_file(file_path: str) -> dict:
     :return: Dictionary containing the YAML file content.
     """
     try:
-        with open(file_path, 'r') as file:
-            return yaml.safe_load(file)
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
     except Exception as e:
         raise AppException(e, sys) from e
